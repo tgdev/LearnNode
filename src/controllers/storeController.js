9277087ade019d940269faf969ec303ef57fdb2f
Tom Gillard <tomgillar@gmail.com>
2017-08-13 16:27:16 +1000

    [MOD 4] - Adding routes and updating controller and views to allow editing of existing stores. Updating TEMP viewStore view to dynamically display store info from controller action

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -5,6 +5,12 @@ exports.homePage = (req, res) => {
   res.render('index', { title: 'Home' });
 };
 
+exports.getStores = async (req, res) => {
+  // query db for list of stores
+  const stores = await Store.find();
+  res.render('Stores', { title: 'Stores', stores });
+};
+
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add store' });
 };
@@ -12,18 +18,26 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
 
-  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-  res.redirect(`/store/${store.slug}`);
+  req.flash('success', `Successfully created <strong>${store.name}</strong>. Care to leave a review?`);
+  res.redirect(`/stores/${store.slug}`);
 };
 
-exports.getStores = async (req, res) => {
-  // query db for list of stores
-  const stores = await Store.find();
-  res.render('Stores', { title: 'Stores', stores });
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store });
 };
 
-exports.viewStore = (req, res) => {
-  res.render('viewStore', {
-    title: req.params.slug
-  });
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new store instead of old one
+    runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.viewStore = async (req, res) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  res.render('viewStore', { title: store.name, store });
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-// Do work here
+// View
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/:slug', storeController.viewStore);
 
+// Create
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
 
-router.get('/store/:slug', storeController.viewStore);
+// Update
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
 
 module.exports = router;

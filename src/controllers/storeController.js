9ae357705fd0d3ff5a1f1a340276495767133cfc
Tom Gillard <tomgillar@gmail.com>
2017-08-13 15:00:15 +1000

    [MOD 4] - Creating async/await for controller to pass form data to store via Store schema. using catchError middleware to handle exceptions

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Home' });
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add store' });
+};
+
+exports.createStore = async (req, res) => {
+  // save to mongoose
+  const store = new Store(req.body);
+  await store.save();
+
+  res.redirect('/');
+
 };

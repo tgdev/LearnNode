35c2c70c4a755c0416ec07e9fcc92955e3e97d6e
Tom Gillard <tomgillar@gmail.com>
2017-08-13 14:52:52 +1000

    [MOD 2] - Creating basic route, controller and index template as part of Core concepts module

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const storeController = require('../controllers/storeController');
+
+// Do work here
+router.get('/', storeController.homePage);
+
+module.exports = router;
